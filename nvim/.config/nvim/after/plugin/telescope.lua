c4c054e9319be6b431943a9931fe6f6bed0ede56
Konrad Barrek (Efix) <konrad.trade@gmail.com>
2024-02-01 21:49:36 +0100

    feat: added killer move to vim

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.api.nvim_set_keymap('n', '<leader>g', [[:lua require('telescope.builtin').grep_string({ search = vim.fn.expand("<cword>") })<CR>]], { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>ps', function() 
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
